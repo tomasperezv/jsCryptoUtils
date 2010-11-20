b5b06f613c9e50f457229484574c5a2cb44e974d
tomasperezv <tomasperezv@atenea.(none)>
2010-11-21 00:11:15 +0100

    Fixed code style.

--- utils/debug/cryptoConsole.js
+++ utils/debug/cryptoConsole.js
@@ -46,32 +46,31 @@ var cryptoConsole = {
      * @param {int} logLevel
      */
     log: function(input, logLevel) {
-	if (typeof logLevel === 'undefined') {
-	   var logLevel = this.LOG_LEVEL_DEBUG; 
-	}
-	this._consoleLog(input, logLevel);
+		if (typeof logLevel === 'undefined') {
+			var logLevel = this.LOG_LEVEL_DEBUG; 
+		}
+		this._consoleLog(input, logLevel);
     },
     
     /**
      * @param {object} input
      */
     info: function(input) {
-	this._consoleLog(input, this.LOG_LEVEL_INFO);
+		this._consoleLog(input, this.LOG_LEVEL_INFO);
     },
-    
     
     /**
      * @param {int} input
      */
     toBinaryString: function(input) {
-	return this.toBaseX(input, this.BASE_BIN);
+		return this.toBaseX(input, this.BASE_BIN);
     },
     
     /**
      * @param {int} input
      */
     toHexString: function(input) {
-	return this.toBaseX(input, this.BASE_HEX);
+		return this.toBaseX(input, this.BASE_HEX);
     },
     
     /**
@@ -79,24 +78,23 @@ var cryptoConsole = {
      * @param {int} base
      */
     toBaseX: function(input, base) {
-	input = parseInt(input);
-	var inputAsBaseX = input.toString(base);
-	
-	return inputAsBaseX;	
+		input = parseInt(input);	
+		var inputAsBaseX = input.toString(base);
+		return inputAsBaseX;	
     },
 
     /**
      * @param {Array} arrayInput
      * @param {string} description
      */
-     logArrayAsBin: function(arrayInput, description) {
-	var outputArray = new Array();
-	for (var key in arrayInput) {
-	    var input = arrayInput[key];
-	    var inputAsBin = this.toBinaryString(input);
-	    outputArray[key] = inputAsBin;
-	}
-	this.logArray(outputArray, description);
+	logArrayAsBin: function(arrayInput, description) {
+		var outputArray = new Array();
+		for (var key in arrayInput) {
+			var input = arrayInput[key];
+			var inputAsBin = this.toBinaryString(input);
+			outputArray[key] = inputAsBin;
+		}
+		this.logArray(outputArray, description);
      },
 
     /**
@@ -104,75 +102,73 @@ var cryptoConsole = {
      * @param {string} description
      */
      logArray: function(arrayInput, description) {
-	var outputString = description + ' ';
-	for (var key in arrayInput) {
-	    var input = arrayInput[key];
-	    outputString += ' ' + input;
-	}
-	this.log(outputString);
+		var outputString = description + ' ';
+		for (var key in arrayInput) {
+			var input = arrayInput[key];
+			outputString += ' ' + input;
+		}
+		this.log(outputString);
      },
-
 
     /**
      * @param {string|object} input
      * @param {int} logLevel
      */
     _consoleLog: function(input, logLevel) {
-	if ( this._isConsoleEnabled() && this._isDebugEnabled()) {
+		if ( this._isConsoleEnabled() && this._isDebugEnabled()) {
 	    
-	    if(typeof input === 'object') {
-		logLevel = this.LOG_LEVEL_INFO;
-	    }
+		    if(typeof input === 'object') {
+				logLevel = this.LOG_LEVEL_INFO;
+			}
 	    
-	    switch(logLevel) {
-		default:
-		case this.LOG_LEVEL_DEBUG: {
-		    console.debug(input);
-		    break;
+			switch(logLevel) {
+				default:
+				case this.LOG_LEVEL_DEBUG: {
+					console.debug(input);
+					break;
+				}
+				case this.LOG_LEVEL_INFO: {
+					console.info(input);
+					break;
+				}
+				case this.LOG_LEVEL_WARN: {
+					console.warn(input);
+					break;
+				}
+				case this.LOG_LEVEL_ERROR: {
+					console.error(input);
+					break;
+				}		
+			}
 		}
-		case this.LOG_LEVEL_INFO: {
-		    console.info(input);
-		    break;
-		}
-		case this.LOG_LEVEL_WARN: {
-		    console.warn(input);
-		    break;
-		}
-		case this.LOG_LEVEL_ERROR: {
-		    console.error(input);
-		    break;
-		}		
-	    }
-	}
     },
     
     /**
      * @return {boolean} Check if the console object is present.
      */
     _isConsoleEnabled: function() {	
-	/*
-	 * The following solution is equivalent to:
-	 *  
-	 * typeof window.console !== 'undefined' && 
-	 * typeof window.console.log !== 'undefined
-	 */
-	var consoleEnabled = ('console' in window && 'log' in window.console);
-	
-	return consoleEnabled;
+		/*
+		 * The following solution is equivalent to:
+		 *  
+		 * typeof window.console !== 'undefined' && 
+		 * typeof window.console.log !== 'undefined
+		 */
+		var consoleEnabled = ('console' in window && 'log' in window.console);
+		return consoleEnabled;
     },
     
     /**
      * @return {boolean} Check if the debug mode is enabled.
      */
     _isDebugEnabled: function() {
-	return this._enabled;
+		return this._enabled;
     },
     
     enableDebug: function() {
-	this._enabled = true;
+		this._enabled = true;
     },
     
     disableDebug: function() {
-	this._enabled = false;
+		this._enabled = false;
     }
 }
