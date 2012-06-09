dc6ac99a0d14c97c3ac456235eb67fa0722a9ce9
Tomas Perez <tom@0x101.com>
2012-06-10 00:41:29 +0400

    Adding the documentation.

---
+++ generate.sh
@@ -0,0 +1,3 @@
+cd ../jsdoc_toolkit-2.4.0/
+java -jar jsrun.jar app/run.js -a -t=templates/github-project -d=../jsCryptoUtils/ -projectTitle="jsCryptoUtils" -projectLink="http://github.com/tomasperezv/jsCryptoUtils" ../../jsCryptoUtils/
+cd -
